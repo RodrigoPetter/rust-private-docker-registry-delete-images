648034bc20ae5f38d444a70b050f7014338d75f5
Rodrigo Petter Daniel <rodrigo-daniel@defensoria.rs.gov.br>
2023-03-13 22:47:09 -0300

    feat: placeholder for avaliable_repositories

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 use std::{io, process::exit};
 
+const SCAN_ID: u16 = 998;
+const GC_ID: u16 = 998;
+const EXIT_ID: u16 = 0;
+const FIXED_OPTIONS: [(u16, &str); 3] = [
+    (
+        SCAN_ID,
+        "Scan all repositories size (this task can take several minutes)",
+    ),
+    (GC_ID, "Run Garbage Collection"),
+    (EXIT_ID, "Exit"),
+];
+
 fn main() {
-    const SCAN_ID: u16 = 998;
-    const GC_ID: u16 = 998;
-    const EXIT_ID: u16 = 0;
-    const FIXED_OPTIONS: [(u16, &str); 3] = [
-        (
-            SCAN_ID,
-            "Scan all repositories size (this task can take several minutes)",
-        ),
-        (GC_ID, "Run Garbage Collection"),
-        (EXIT_ID, "Exit"),
-    ];
+    let avaliable_repositories = get_repositories_from_registry();
 
     loop {
         println!("List of avaliable repositories and options:");
 
+        //TODO: Check how to concat/extend the Vec with the array so two for loops are not needed
+        for (id, text) in avaliable_repositories.iter() {
+            print_option(&id, &text);
+        }
         for (id, text) in FIXED_OPTIONS {
             print_option(&id, &text);
         }
@@ -54,4 +60,14 @@ fn read_input(message: &str) -> u16 {
             }
         };
     }
+}
+
+fn get_repositories_from_registry() -> Vec<(u16, String)> {
+    //TODO: get real repositories from https://xxx.xx/v2/_catalog
+    return vec![
+        (1, String::from("Placeholder 1")),
+        (2, String::from("Placeholder 2")),
+        (3, String::from("Placeholder 3")),
+        (4, String::from("Placeholder 4")),
+    ];
 }

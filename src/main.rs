3280eec1bcef4175e7abd877e130ab4d9ec5bffb
Rodrigo Petter Daniel <rodrigo-daniel@defensoria.rs.gov.br>
2023-03-13 21:13:59 -0300

    feat: permite ler inputs numericos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let _test = read_input("Select one repository:");
+    }
+}
+
+fn read_input(message: &str) -> u16 {
+    loop {
+        println!("{message}");
+
+        let mut input = String::new();
+
+        io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read stdin");
+
+        return match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a valid input. Try again.");
+                continue;
+            },
+        };
+    }
 }

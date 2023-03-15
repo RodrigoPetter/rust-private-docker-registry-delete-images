17ce582f9e4cc1e6c59df517997f1d1bd29ed176
Rodrigo Petter Daniel <rodrigo-daniel@defensoria.rs.gov.br>
2023-03-14 22:29:54 -0300

    refactor: consts to enum

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use std::{io, process::exit};
 
-const SCAN_ID: u16 = 998;
-const GC_ID: u16 = 998;
-const EXIT_ID: u16 = 0;
-const FIXED_OPTIONS: [(u16, &str); 3] = [
-    (
-        SCAN_ID,
-        "Scan all repositories size (this task can take several minutes)",
-    ),
-    (GC_ID, "Run Garbage Collection"),
-    (EXIT_ID, "Exit"),
-];
+const COMMANDS: [Command; 3] = [Command::Scan, Command::GC, Command::Exit];
+enum Command {
+    Scan,
+    GC,
+    Exit,
+}
+
+impl TryFrom<u16> for Command {
+    type Error = ();
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        match value {
+            value if value == 998 => Ok(Command::Scan),
+            value if value == 999 => Ok(Command::GC),
+            value if value == 0 => Ok(Command::Exit),
+            _ => Err(()),
+        }
+    }
+}
 
 fn main() {
     let avaliable_repositories = get_repositories_from_registry();
@@ -18,19 +26,29 @@ fn main() {
     loop {
         println!("List of avaliable repositories and options:");
 
-        //TODO: Check how to concat/extend the Vec with the array so two for loops are not needed
         for (id, text) in avaliable_repositories.iter() {
-            print_option(&id, &text);
+            print_option(id, &text);
         }
-        for (id, text) in FIXED_OPTIONS {
-            print_option(&id, &text);
+        for command in COMMANDS {
+            match command {
+                Command::Scan => print_option(
+                    &998,
+                    "Scan all repositoriessize (this task can take several minutes)",
+                ),
+                Command::GC => print_option(&999, "Run Garbage Collection"),
+                Command::Exit => print_option(&0, "Exit"),
+            }
         }
 
         let selected = read_input("Select an option:");
 
-        match selected {
-            EXIT_ID => exit(0),
-            _ => {
+        match Command::try_from(selected) {
+            Ok(cmd) => match cmd {
+                Command::Scan => todo!(),
+                Command::GC => todo!(),
+                Command::Exit => exit(0),
+            },
+            Err(_) => {
                 println!("Not a valid option. Try again.");
                 continue;
             }
